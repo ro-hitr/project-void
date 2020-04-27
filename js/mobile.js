6243462eb0ff530869e977cfea7e06f9f788d0b7
ro-hitr <rohitthefun@gmail.com>
2020-04-28 04:03:50 +0530

    change mobile.js format

--- js/mobile.js
+++ js/mobile.js
@@ -1,21 +1,24 @@
-var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-var element = document.getElementById('text');
-if (isMobile) 
-{
-    $(document).ready(function(){
-        'use strict';
-        $('.text').css({
-            'font-size':'60px',
-            'width':'100%'
+$(document).ready(function(){
+    var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    var element = document.getElementById('text');
+    if (isMobile) 
+    {
+        $(document).ready(function(){
+            'use strict';
+            $('.text').css({
+                'font-size':'60px',
+                'width':'100%',
+                'text-align':'center'
+            });
+            $('.clock').css({
+                'width': '60vw',
+                'height': '60vw',
+                'margin-left': '20vw',
+                'margin-right': '20vw'
+            });
         });
-        $('.clock').css({
-            'width': '60vw',
-            'height': '60vw',
-            'margin-left': '20vw',
-            'margin-right': '20vw'
-        });
-    });
-}
+    }
+});
 /*
 else 
 {
