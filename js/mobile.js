3ce229fde656558aadc7cdbfb6aa6e4f90bcfb2c
ro-hitr <rohitthefun@gmail.com>
2020-04-28 03:08:23 +0530

    add mobile js

---
+++ js/mobile.js
@@ -0,0 +1,22 @@
+var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+var element = document.getElementById('text');
+if (isMobile) 
+{
+    $(document).ready(function(){
+        'use strict';
+        $('.text').css({
+            'font-size':'60px',
+            'width':'100%'
+        });
+        $('.clock').css({
+            'width': '60vw',
+            'height': '60vw',
+            'margin-left': '20vw',
+            'margin-right': '20vw',
+            'height': 'width'
+        });
+} 
+/**else 
+{
+	element.innerHTML = "You are using Desktop";
+}*/
